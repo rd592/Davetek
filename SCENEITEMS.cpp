ff32a6518558b2af18fe57273f409b86ec2fe0fc
rd592 <66571852+rd592@users.noreply.github.com>
2024-04-24 09:29:55 +0100

    Update SCENEITEMS.cpp

--- SCENEITEMS.cpp
+++ SCENEITEMS.cpp
@@ -1,6 +1,6 @@
 #include "SCENEITEMS.h"
 
-
+//constructor for buttons
 Button :: Button(LCD1602 lcd, int nextScene, const char* buttonText, int columnLoc, int rowLoc){
         _columnLoc = columnLoc;
         _rowLoc = rowLoc;
@@ -8,10 +8,23 @@ Button :: Button(LCD1602 lcd, int nextScene, const char* buttonText, int columnL
         _buttonText = buttonText;
 }
 
+//constructor for text
 Button :: Button(LCD1602 lcd, const char* buttonText, int columnLoc, int rowLoc){
         _columnLoc = columnLoc;
         _rowLoc = rowLoc;
         _buttonText = buttonText;
+
+}
+
+//constructor for custom characters
+Button :: Button(LCD1602 lcd, const int *customChar, int columnLoc, int rowLoc){
+        _columnLoc = columnLoc;
+        _rowLoc = rowLoc;
+        _customChar = customChar;
+        
+        //these statements are used to find where to put the custom character
+
+        //lcd.loadCustomChar(location, customChar);
 
 }
 
