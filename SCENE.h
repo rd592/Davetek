fda5ac91dde1b00082161229bfcdfc269c431111
rd592 <66571852+rd592@users.noreply.github.com>
2024-04-24 09:30:11 +0100

    Update SCENE.h

--- SCENE.h
+++ SCENE.h
@@ -1,6 +1,9 @@
 #include "LCD1602.h"
 #include "mbed.h"
 #include "SCENEITEMS.h"
+#include <vector>
+
+using namespace std;
 
 class Scene {
 
@@ -8,18 +11,27 @@ class Scene {
     int _sceneID;
     bool active;
 
-
+    /*
         Button _buttonArray[10];//tracks the buttons in a scene
         Button _textArray[10];
+        Button _charArray[16];//custom chars
+    */
+    
+        vector<Button> _buttonArray;
+        vector<Button> _textArray;
+        vector<Button> _charArray;
 
         int textNum = 0;
         int buttonNum = 0;//number of buttons
+        int charNum = 0;//number of custom chars
 
         Scene();
         Scene(int sceneID);
 
 
-        void NewText(LCD1602 lcd,  const char *text, int column, int row);
+        void NewText(LCD1602 lcd,  const char *text, int column, int row); //used to print any basic character
+        void NewChar(LCD1602 lcd, const int *customChar, int column, int row); //specifically for custom characters
+
         void NewButton(LCD1602 lcd, int nextScene, const char* buttonText, int column, int row);
 
         void ClearScene(LCD1602 lcd);
