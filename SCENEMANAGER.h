36b8fb77edeba48a8b5c318faa05b5fb70b59fe4
rd592 <66571852+rd592@users.noreply.github.com>
2024-04-24 09:29:17 +0100

    Update SCENEMANAGER.h

--- SCENEMANAGER.h
+++ SCENEMANAGER.h
@@ -11,14 +11,16 @@ class SceneManager{
 public:
 //number of scenes is capped at 50 right now. making the array too large overlaps with other memory addresses. 
 //I dont know how to fix this right now.
-    Scene sceneArray[50];
+    Scene sceneArray[20];
     int sceneNum = 0;
 
     SceneManager(LCD1602 lcd);
 
+    //these functions can be followed down to sceneitems through scene. 
     void NewScene(int ID);
     void NewButton(int sceneID, int nextScene, const char *text, int column, int row);
     void NewText(int sceneID, const char *text, int column, int row);
+    void NewChar(int sceneID, const int *customChar, int column, int row); //custom char
 
     void DisplaySceneByID(int ID);
     //Scene GetScene();
